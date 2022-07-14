3233bdd54e58c64ed4770965cd1a3467f6de6dcd
Krish Bhatt <krishbhatt1217@gmail.com>
2022-07-14 17:47:54 +0530

    calculateBill completed

--- Billing_System.c
+++ Billing_System.c
@@ -14,17 +14,19 @@ typedef struct customerData{
     char name[30];
     char number[12];
     char email[40];
+    char date[10];
 } customerData;
 
 void preferredOption(int *choice){
-    for(int i = 0; i < 21; i++){
-        if(i == 11){
-            printf("HJHP Restaurant");
-        }
-        else{
-            printf("=");
-        }
-    }
+    // for(int i = 0; i < 21; i++){
+    //     if(i == 11){
+    //         printf("QUINTET Restaurant");
+    //     }
+    //     else{
+    //         printf("=");
+    //     }
+    // }
+    printf("==========QUINTET Restaurant==========\n");
     printf("\n");
     printf("\nPlease Select Your Preferred Option:\n");
     printf("1. Generate Invoice\n");
@@ -38,31 +40,39 @@ void preferredOption(int *choice){
 
 // HARSHIT, YOU HAVE TO COMPLETE THIS DESIGN FUNCTION NAMED "invoiceDesign"
 // USE customerNo TO GET PARTICULAR CUSTOMER'S DETAILS
-void invoiceDesign(billData *bill, customerData *customer, int *total, int customerNo, int itemType){
-
+void invoiceDesign(billData **bills, customerData *customer, int *total, int customerNo, int itemType){
+    int main();
 }
 
 // HARSH, YOU HAVE TO COMPLETE THIS CALCULATION FUNCTION NAMED "calculateBill"
 // CALCULATE THE TOTAL WITHOUT GST INCLUDED
-void calculateBill(billData *bill, int *total, int customerNo, int itemType){
+void calculateBill(billData **bills, int *total, int customerNo, int itemType){
     int totalBill = 0;
     for(int i = 0; i < itemType; i++){
-        totalBill += bill[i].quantity * bill[i].unitPrice;
+        totalBill += bills[customerNo][i].quantity * bills[customerNo][i].unitPrice;
     }
-    *total = totalBill;
-    // printf("\nTotal Bill: %d\n", totalBill);
+    total[customerNo] = totalBill;
+    printf("\nTotal Bill: %d\n", totalBill);
     return;
 }
 
-void showAllInvoices(){
-
+void showAllInvoices(billData **bills, customerData *customers, int *itemsType, int *customerNo, int *total){
+    for(int i = 0; i < customerNo; i++){
+        
+    }
+    //TODO 
 }
 
-void searchInvoice(){
-
+void searchInvoice(billData **bills, customerData *customers, int *itemsType, int *customerNo){
+    //TODO 
+    
+    
+    printf("\nEnter the Customer Name : ");
+    scanf("%s", &customer);
+    
 }
 
-void invoice(customerData *customer, int *customerNo, int *total){
+void invoice(customerData *customer, int *customerNo, int *total, billData **bills){
     int itemsType = 0;
     printf("\nEnter Your Name: ");
     scanf("%s", customer[*customerNo].name);
@@ -71,11 +81,10 @@ void invoice(customerData *customer, int *customerNo, int *total){
     printf("\nEnter Your Email: ");
     scanf("%s", customer[*customerNo].email);
     printf("\n");
-    printf("\nEnter the number of Different items you hove ordered: ");
+    printf("\nEnter the number of Different items you have ordered: ");
     scanf("%d", &itemsType);
-    billData *bills = calloc(100, sizeof(billData));
     billData *bill = calloc(itemsType, sizeof(billData));
-    bills[*customerNo] = *bill;
+    bills[*customerNo] = bill;
     for(int i = 0; i < itemsType; i++){
         printf("\nEnter the name of the item %d: ", i+1);
         scanf("%s", bill[i].name);
@@ -84,8 +93,8 @@ void invoice(customerData *customer, int *customerNo, int *total){
         printf("Enter the unit price of the item %d: ", i+1);
         scanf("%d", &bill[i].unitPrice);
     }
-    calculateBill(bill, total, *customerNo, itemsType);
-    invoiceDesign(bill, customer, total, *customerNo, itemsType);
+    calculateBill(bills, total, *customerNo, itemsType);
+    invoiceDesign(bills, customer, total, *customerNo, itemsType);
     *customerNo++;
     return;
 }
@@ -96,16 +105,17 @@ int main(){
     customerData *customers = calloc(100, sizeof(customerData));
     // customerArr[customerNo] = *customers;
     int *total = calloc(100, sizeof(int));
+    billData **bills = calloc(100, sizeof(billData *));
     while(choice != 1 && choice != 2 && choice != 3 && choice != 4)
     {
         preferredOption(&choice);
     }
     switch(choice){
         case 1:
-            invoice(customers, &customerNo, total);
+            invoice(customers, &customerNo, total, bills);
             break;
         case 2:
-            showAllInvoices();
+            // showAllInvoices();
             break;
         case 3:
             searchInvoice();
