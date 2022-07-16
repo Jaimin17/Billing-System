1b0b6748d83f694cc9b88f65662704f990f352bf
Harshit Dodiya <106812242+Harrine@users.noreply.github.com>
2022-07-16 20:43:28 +0530

    Update Billing_System.c
    Allignment bug fixes

--- Billing_System.c
+++ Billing_System.c
@@ -34,26 +34,33 @@ void preferredOption(int *choice){
 // HARSHIT, YOU HAVE TO COMPLETE THIS DESIGN FUNCTION NAMED "invoiceDesign"
 // USE customerNo TO GET PARTICULAR CUSTOMER'S DETAILS
 void invoiceDesign(billData **bills, customerData *customer, int *total, int customerNo){
-    
+    int l=0,a;
     printf("\n=============QUINTET Restaurant=============");
-    // printf("\n%s",customer[customerNo].date);
+    printf("\n%s",customer[customerNo].date);
     printf("\nInvoice To: %s",customer[customerNo].name);
     printf("\n============================================");
-    printf("\nItem\t\tQty\t\t  Total");
+    printf("\nItem\t\t    Qty\t\tTotal");
     printf("\n============================================");
-    for(int i = 0; i < itemType; i++){
-        printf("\n%s\t\t%d\t\t  %d",bills[customerNo][i].name,bills[customerNo][i].quantity,bills[customerNo][i].quantity * bills[customerNo][i].unitPrice);
-    }
+        for(int i = 0; i<customer[customerNo].itemType; i++){
+           l=strlen(bills[customerNo][i].name);
+           printf("\n%s",bills[customerNo][i].name);
+           for(int j = 0; j < 20-l; j++)
+           {
+                printf(" ");
+           }
+           printf("%d",bills[customerNo][i].quantity);
+           printf("\t\t %d",bills[customerNo][i].quantity*bills[customerNo][i].unitPrice); 
+        }
     printf("\n============================================");
-    printf("\nSub Total\t\t\t  %d",total[customerNo]);
-    printf("\nDiscount(@10)\t\t          %f",(float)(total[customerNo]*10/100));
-    printf("\n             \t\t          ===========");
-    printf("\nNet Total    \t\t          %d",total[customerNo]-total[customerNo]*10/100);
-    printf("\nCGST (@9%)   \t\t          %f",(float)(total[customerNo]*9/100));
-    printf("\nSGST (@9%)   \t\t          %f",(float)(total[customerNo]*9/100));
+    printf("\nSub Total\t\t\t %d",total[customerNo]);
+    printf("\nDiscount(@10)\t\t         %f",(float)(total[customerNo]*10/100));
+    printf("\n             \t\t         ============");
+    printf("\nNet Total    \t\t         %d",total[customerNo]-total[customerNo]*10/100);
+    printf("\nCGST (@9%)   \t\t         %f",(float)(total[customerNo]*9/100));
+    printf("\nSGST (@9%)   \t\t         %f",(float)(total[customerNo]*9/100));
     printf("\n============================================");
-    printf("\nGrand Total  \t\t          %f",(float)(total[customerNo]+total[customerNo]*9/100+total[customerNo]*9/100));
-    printf("\n============================================");
+    printf("\nGrand Total  \t\t         %f",(float)(total[customerNo]+total[customerNo]*9/100+total[customerNo]*9/100));
+    printf("\n============================================\n");
 }
 
 // HARSH, YOU HAVE TO COMPLETE THIS CALCULATION FUNCTION NAMED "calculateBill"
@@ -87,8 +94,9 @@ void searchInvoice(billData **bills, customerData *customers, int *customerNo, i
     return;
 }
 
+
 void invoice(customerData *customer, int *customerNo, int *total, billData **bills){
-    *customer[*customerNo].date = __DATE__;
+    strcpy(customer[*customerNo].date, __DATE__ );
     lable4:
         gets(customer[*customerNo].name);
         printf("\nEnter Your Name: ");
